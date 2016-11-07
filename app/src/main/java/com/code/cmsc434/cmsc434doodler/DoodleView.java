b5c6f6d65608e4c3fdf1748e2a6b56fcff1cbbd3
seiashun <thomasj.yang94@gmail.com>
2016-11-07 13:58:43 -0500

    Added Google Material Design Icons for Undo/Redo Added Size and Opacity sliders Added Undo and Redo Options
    Changed slider label size to smaller font
    Changed DoodleView size to smaller size

--- app/src/main/java/com/code/cmsc434/cmsc434doodler/DoodleView.java
+++ app/src/main/java/com/code/cmsc434/cmsc434doodler/DoodleView.java
@@ -9,32 +9,122 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by thomasyang on 11/2/16.
  */
 
 public class DoodleView extends View {
 
+    List<Stroke> _strokes = new ArrayList<Stroke>();
+    List<Stroke> _undo = new ArrayList<Stroke>();
     private Paint _paintDoodle = new Paint();
     private Path _path = new Path();
+
+    private int color;
+    private float width;
 
     public DoodleView(Context context) {
         super(context);
         init(null, 0);
+        color = Color.BLACK;
+        width = 4.0f;
     }
 
     public DoodleView(Context context, AttributeSet attrs) {
         super(context, attrs);
         init(attrs, 0);
+        color = Color.BLACK;
+        width = 4.0f;
     }
 
     public DoodleView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init(attrs, defStyleAttr);
+        color = Color.BLACK;
+        width = 4.0f;
+    }
+
+    public void setAlpha(int percent) {
+        int color = _paintDoodle.getColor();
+        int alpha = (int) (255 * (percent / 100.0));
+        int red = Color.red(color);
+        int green = Color.green(color);
+        int blue = Color.blue(color);
+
+        setBrushColor(alpha, red, green, blue);
+    }
+
+    public void setRed(int percent) {
+        int color = _paintDoodle.getColor();
+        int alpha = Color.alpha(color);
+        int red = (int) (255 * (percent / 100.0));
+        int green = Color.green(color);
+        int blue = Color.blue(color);
+
+        setBrushColor(alpha, red, green, blue);
+    }
+
+    public void setGreen(int percent) {
+        int color = _paintDoodle.getColor();
+        int alpha = Color.alpha(color);
+        int red = Color.red(color);
+        int green = (int) (255 * (percent / 100.0));
+        int blue = Color.blue(color);
+
+        setBrushColor(alpha, red, green, blue);
+
+    }
+
+    public void setBlue(int percent) {
+        int color = _paintDoodle.getColor();
+        int alpha = Color.alpha(color);
+        int red = Color.red(color);
+        int green = Color.green(color);
+        int blue = (int) (255 * (percent / 100.0));
+
+        setBrushColor(alpha, red, green, blue);
+
+    }
+
+    public void setSize(int percent) {
+        width = 4.0f + (36.0f * (float)(percent / 100.0));
+        _paintDoodle.setStrokeWidth(width);
+    }
+
+    public void undo() {
+        if (_strokes.size() > 0) {
+            _undo.add(_strokes.remove(_strokes.size() - 1));
+            invalidate();
+        }
+    }
+
+    public void redo() {
+        if (_undo.size() > 0) {
+            _strokes.add(_undo.remove(_undo.size() - 1));
+            invalidate();
+        }
+    }
+
+    public int canUndo() {
+        return _strokes.size();
+    }
+
+    public int canRedo() {
+        return _undo.size();
+    }
+
+    private void setBrushColor(int a, int r, int g, int b) {
+        _paintDoodle.setARGB(a, r, g, b);
+        color = _paintDoodle.getColor();
+        invalidate();
     }
 
     public void clearScreen() {
-        _path.reset();
+        _strokes.clear();
+        _undo.clear();
         invalidate();
     }
 
@@ -49,7 +139,9 @@ public class DoodleView extends View {
     public void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawPath(_path, _paintDoodle);
+        for (Stroke s : _strokes) {
+            canvas.drawPath(s.path, s.doodle);
+        }
     }
 
     @Override
@@ -59,15 +151,34 @@ public class DoodleView extends View {
 
         switch(motionEvent.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                _path.moveTo(touchX, touchY);
+                _strokes.add(new Stroke(color, width));
+                _strokes.get(_strokes.size() - 1).path.moveTo(touchX, touchY);
+                //_path.moveTo(touchX, touchY);
                 break;
             case MotionEvent.ACTION_MOVE:
-                _path.lineTo(touchX, touchY);
+                _strokes.get(_strokes.size() - 1).path.lineTo(touchX, touchY);
+                //_path.lineTo(touchX, touchY);
                 break;
             case MotionEvent.ACTION_UP:
+                _undo.clear();
                 break;
         }
         invalidate();
         return true;
+    }
+
+    private class Stroke {
+        private Paint doodle;
+        private Path path;
+
+        public Stroke(int color, float width) {
+            doodle = new Paint();
+            path = new Path();
+
+            doodle.setColor(color);
+            doodle.setAntiAlias(true);
+            doodle.setStyle(Paint.Style.STROKE);
+            doodle.setStrokeWidth(width);
+        }
     }
 }
--- app/src/main/java/com/code/cmsc434/cmsc434doodler/MainActivity.java
+++ app/src/main/java/com/code/cmsc434/cmsc434doodler/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,12 +15,136 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button b_clear = (Button) findViewById(R.id.id_button_clear);
+        ImageButton b_undo = (ImageButton) findViewById(R.id.id_undo_button);
+        ImageButton b_redo = (ImageButton) findViewById(R.id.id_redo_button);
+
+        final SeekBar seek_alpha = (SeekBar) findViewById(R.id.id_seek_alpha);
+        final SeekBar seek_red = (SeekBar) findViewById(R.id.id_seek_red);
+        final SeekBar seek_green = (SeekBar) findViewById(R.id.id_seek_green);
+        final SeekBar seek_blue = (SeekBar) findViewById(R.id.id_seek_blue);
+        final SeekBar seek_size = (SeekBar) findViewById(R.id.id_seek_size);
 
         b_clear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 DoodleView dv = (DoodleView) findViewById(R.id.id_doodleview);
                 dv.clearScreen();
+
+                seek_alpha.setProgress(100);
+                seek_red.setProgress(0);
+                seek_green.setProgress(0);
+                seek_blue.setProgress(0);
+                seek_size.setProgress(0);
+            }
+        });
+
+        b_undo.setOnClickListener(new ImageButton.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                DoodleView dv = (DoodleView) findViewById(R.id.id_doodleview);
+                dv.undo();
+            }
+        });
+
+        b_redo.setOnClickListener(new ImageButton.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DoodleView dv = (DoodleView) findViewById(R.id.id_doodleview);
+                dv.redo();
+            }
+        });
+
+        seek_alpha.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                DoodleView dv = (DoodleView) findViewById(R.id.id_doodleview);
+                dv.setAlpha(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seek_red.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                DoodleView dv = (DoodleView) findViewById(R.id.id_doodleview);
+                dv.setRed(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seek_blue.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                DoodleView dv = (DoodleView) findViewById(R.id.id_doodleview);
+                dv.setBlue(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seek_green.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                DoodleView dv = (DoodleView) findViewById(R.id.id_doodleview);
+                dv.setGreen(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seek_size.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                DoodleView dv = (DoodleView) findViewById(R.id.id_doodleview);
+                dv.setSize(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
             }
         });
     }
